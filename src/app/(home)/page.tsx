279bda7b7f346fefea1e5bdb249d0ba6d82c21ca
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-23 22:24:48 +0900

    useState, useEffect없이 서버 컴포넌트에서 데이터 로딩 처리 with fetch함수

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,15 @@
-"use client"
-import { useEffect, useState } from "react";
+//fetch 함수
+//server components -> data 직접 서버에서 비동기적으로 가져오기에 useState, useEffect필요x
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 5000))//getmovies 함수가 여기에서 5초동안 멈추고(로딩) 진행
+  const response = await fetch(`${process.env.NEXT_PUBLIC_MOVIE_API_KEY}`)
+  const json = await response.json()
+  return json
+}
 
-
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-    const json = await response.json()
-    setMovies(json);
-    setIsLoading(false)
-  }
-  useEffect(() => {
-    getMovies();
-  }, [])
-
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
-    <div>{isLoading ? "Loading" : JSON.stringify(movies)}</div>
+    <div> {JSON.stringify(movies)}</div>
   )
 }
