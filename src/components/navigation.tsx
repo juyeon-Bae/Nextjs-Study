077479be42ece061a9c952824a9598b769f63323
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-23 20:16:10 +0900

    navigation

---
+++ src/components/navigation.tsx
@@ -0,0 +1,20 @@
+"use client"
+import Link from "next/link" //백엔드에서 render되고 프론트에서 hydrate됨
+import { usePathname } from "next/navigation"
+import { useState } from "react";
+export default function Navigation() {
+    const path = usePathname();
+    const [count, setCount] = useState(0)
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">Home</Link>{path === "/" ? "📌" : ""}
+                </li>
+                <li><Link href="/about-us">about Us</Link> {path === "/about-us" ? "💡" : ""}</li>
+                <li><button onClick={() => setCount(c => c + 1)}>{count}</button></li>
+            </ul>
+        </nav>
+    )
+}
