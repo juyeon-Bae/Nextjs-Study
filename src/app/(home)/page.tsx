0d9be654117b2fc174f65dfd3f290beaee207949
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-25 22:41:00 +0900

    loading page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 //fetch 함수
 //server components -> data 직접 서버에서 비동기적으로 가져오기에 useState, useEffect필요x
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000))//getmovies 함수가 여기에서 5초동안 멈추고(로딩) 진행
+  //await new Promise((resolve) => setTimeout(resolve, 1000))//getmovies 함수가 여기에서 1초동안 멈추고(로딩) 진행
   const response = await fetch(`${process.env.NEXT_PUBLIC_MOVIE_API_KEY}`)
   const json = await response.json()
   return json
