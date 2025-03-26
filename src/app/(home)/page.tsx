db643cc5c85f2dca5addaa0a9c9bc3cca722b430
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-26 21:42:03 +0900

    각 id에 해당하는 title 불러오기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 //fetch 함수
 //server components -> data 직접 서버에서 비동기적으로 가져오기에 useState, useEffect필요x
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
 async function getMovies() {
   //await new Promise((resolve) => setTimeout(resolve, 1000))//getmovies 함수가 여기에서 1초동안 멈추고(로딩) 진행
-  const response = await fetch(`${process.env.NEXT_PUBLIC_MOVIE_API_KEY}`)
+  const response = await fetch(API_URL)
   const json = await response.json()
   return json
 }
@@ -10,6 +13,10 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div> {JSON.stringify(movies)}</div>
+    <div>
+      {movies.map(movie => <li key={movie.id}><Link href={
+        `/movies/${movie.id}`}>{movie.title}
+      </Link></li>)}
+    </div>
   )
 }
--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,9 +1,17 @@
-export default function MovieDetail(
+import { API_URL } from "../../../(home)/page"
+async function getMovie(id: String) {
+    const response = await fetch(`${API_URL}/${id}`)
+    return response.json()
+}
+export default async function MovieDetail(
     { params: { id } }:
         { params: { id: string } }) {
+
+    const movie = await getMovie(id);
+
     return (
         <div>
-            <h1>movies {id}</h1>
+            <h1>{movie.title}</h1>
         </div>
     )
 }
