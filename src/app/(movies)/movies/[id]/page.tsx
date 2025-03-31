6f498d01b9af6a56379f85d43c9fad2271e8d044
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-31 22:43:10 +0900

    getMovie, getVideos 함수 지움(컴포넌트로 처리함)

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -2,18 +2,6 @@ import { Suspense } from "react"
 import { API_URL } from "../../../(home)/page"
 import MovieInfo from "@/components/movie-info"
 import MovieVideos from "@/components/movie-videos"
-async function getMovie(id: String) {
-    await new Promise((resolve) => setTimeout(resolve, 10))
-    const response = await fetch(`${API_URL}/${id}`)
-    return response.json()
-}
-
-async function getVideos(id: string) {
-    console.log(`fetching movies: ${Date.now()}`)
-    await new Promise((resolve) => setTimeout(resolve, 10))
-    const response = await fetch(`${API_URL}/${id}/videos`)
-    return response.json()
-}
 
 export default async function MovieDetail(
     { params: { id } }:
