7c4656b5b89a4a02e90e633f5837eeb72654d0d0
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-31 22:40:26 +0900

    getVideos 함수 만들고, movie info, videos 컴포넌트 import

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,17 +1,35 @@
+import { Suspense } from "react"
 import { API_URL } from "../../../(home)/page"
+import MovieInfo from "@/components/movie-info"
+import MovieVideos from "@/components/movie-videos"
 async function getMovie(id: String) {
+    await new Promise((resolve) => setTimeout(resolve, 10))
     const response = await fetch(`${API_URL}/${id}`)
     return response.json()
 }
+
+async function getVideos(id: string) {
+    console.log(`fetching movies: ${Date.now()}`)
+    await new Promise((resolve) => setTimeout(resolve, 10))
+    const response = await fetch(`${API_URL}/${id}/videos`)
+    return response.json()
+}
+
 export default async function MovieDetail(
     { params: { id } }:
         { params: { id: string } }) {
 
-    const movie = await getMovie(id);
-
     return (
         <div>
-            <h1>{movie.title}</h1>
+            <h3>Movie detail page</h3>
+            {/* suspense : component가 await되는 동안 표시할 메세지를 render할 수 있게 해줌, 로딩상태 분리 */}
+            <Suspense fallback={<h1>loading movie info</h1>}>
+                <MovieInfo id={id} />
+            </Suspense>
+
+            <Suspense fallback={<h1>loading movie video</h1>}>
+                <MovieVideos id={id} />
+            </Suspense>
         </div>
     )
 }
