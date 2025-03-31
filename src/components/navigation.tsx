43c7516164cbc03d9fe0fc68ab7e8181184063b7
juyeon Bae <w2316@e-mirim.hs.kr>
2025-03-31 23:11:39 +0900

    CSS Modules

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Link from "next/link" //백엔드에서 render되고 프론트에서 hydrate됨
+import styles from "../app/styles/navigation.module.css"
 import { usePathname } from "next/navigation"
 import { useState } from "react";
 export default function Navigation() {
@@ -7,13 +8,13 @@ export default function Navigation() {
     const [count, setCount] = useState(0)
 
     return (
-        <nav>
-            <ul>
+        <nav className={styles.nav}>
+            <ul className={styles.list}>
                 <li>
                     <Link href="/">Home</Link>{path === "/" ? "📌" : ""}
                 </li>
                 <li><Link href="/about-us">about Us</Link> {path === "/about-us" ? "💡" : ""}</li>
-                <li><button onClick={() => setCount(c => c + 1)}>{count}</button></li>
+                {/* <li><button onClick={() => setCount(c => c + 1)}>{count}</button></li> */}
             </ul>
         </nav>
     )
